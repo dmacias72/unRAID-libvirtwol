1b9334547c20c2b0e88df56f3cb2bf3e1bfabd89
dmacias72 <derekmacias@live.com>
2019-01-06 21:47:43 -0700

    add virtual bmc permissions

--- source/libvirtwol/install/doinst.sh
+++ source/libvirtwol/install/doinst.sh
@@ -5,9 +5,10 @@ FILE="/usr/local/emhttp/plugins/dynamix.vm.manager/VMSettings.page"
 # Update file permissions of scripts
 chmod +0755 /usr/local/emhttp/plugins/libvirtwol/scripts/* \
     /usr/local/emhttp/plugins/libvirtwol/event/* \
-    /etc/rc.d/rc.libvirtwol
+    /etc/rc.d/rc.libvirtwol \
+    /etc/rc.d/rc.virtualbmc
 
-# add stop to shutdown script	
+# add stop to shutdown script
 if ! grep "$SCRIPT" $FILE >/dev/null 2>&1
     then echo -e "\n$SCRIPT" >> $FILE
 fi
